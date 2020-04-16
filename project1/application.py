e931329badde3794780aa78ee1f6b5d449107411
ReetikaSiddantam <reethikasiddantam27@msitprogram.net>
2020-04-16 23:31:27 +0530

    Commiting

--- project1/application.py
+++ project1/application.py
@@ -1,39 +1,62 @@
 import os
 
-from flask import Flask, session
-from flask import Flask,render_template,request
+from flask import Flask, session,url_for,redirect
+from flask import Flask,render_template,request,flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+from model import*
+from  datetime import datetime
 
+
+
+  
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# Check for environment variable
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+
+# # Configure session to use filesystem
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# ab = scoped_session(sessionmaker(bind=engine))
 
-# @app.route("/register")
-# def index():
-#     return render_template("Registartion.html")
 
+       
 @app.route("/")
-def index():
-     return render_template("Registartion.html")
+def index():    
+    return render_template("Registartion.html")
+ 
+      
  
 @app.route("/User",methods = ["GET","POST"])
 def user():
- 
+    
+    Registartion.query.all()
     f = request.form.get("fname")
-    l  = request.form.get("lname")
-    print(f + " "+ l)
-    return render_template("User.html",first = f,last = l)
+    email = request.form.get("Email")
+    p = request.form.get("password")
+    register = Registartion(Firstname = f,Email=email,datetime = datetime.now())
+    try:
+        db.session.add(register)
+        db.session.commit()
+        print(f+" "+email)
+        return render_template("User.html",f=f,email = email)
+    except Exception :
+	    return render_template("error.html", errors = "Details are already given")
+
+
+   
+
+
             
---
+++ project1/model.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Registartion(db.Model):
+    __tablename__ = "Registartion"
+    Firstname = db.Column(db.String, primary_key=True)
+    Email = db.Column(db.String, nullable=False)
+    datetime = db.Column(db.String,nullable= False)
